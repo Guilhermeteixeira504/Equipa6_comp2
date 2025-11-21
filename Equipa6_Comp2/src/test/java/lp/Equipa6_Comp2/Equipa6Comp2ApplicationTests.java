9bf055977b87fa643b6f6b4f2820fbf1522a50a9
Guilhermeteixeira504 <52416@alunos.upt.pt>
2025-11-21 10:27:56 +0000

    projeto comm os packages certos

---
+++ Equipa6_Comp2/src/test/java/lp/Equipa6_Comp2/Equipa6Comp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package lp.Equipa6_Comp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Equipa6Comp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
