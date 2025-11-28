497a37f1d61cb5a2e0c68965b155a3b7663d1a73
carolina15afonso-dot <carolina15afonso@gmail.com>
2025-11-28 12:04:55 +0000

    Voluntario service

---
+++ VoluntarioService.java
@@ -0,0 +1,42 @@
+package lp.Equipa6_Comp2.service;
+
+import org.springframework.stereotype.Service;
+import lp.Equipa6_Comp2.entity.Voluntario;
+import lp.Equipa6_Comp2.repository.VoluntarioRepository;
+import java.util.List;
+
+@Service
+public class VoluntarioService {
+	
+	private final VoluntarioRepository voluntarioRepository;
+	
+	public VoluntarioService(VoluntarioRepository voluntarioRepository) {
+		this.voluntarioRepository = voluntarioRepository;
+	}
+	
+	public List<Voluntario> getAllVoluntarios(){
+		return voluntarioRepository.findAll();
+	}
+	
+	public Voluntario getVoluntario(Long id) {
+		return voluntarioRepository.findById(id).orElse(null);
+	}
+	
+	public Voluntario createVoluntario(Voluntario v) {
+		return voluntarioRepository.save(v);
+	}
+	
+	public Voluntario updateVoluntario(Long id, Voluntario v) {
+		Voluntario existing = voluntarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Voluntário não encontrado"));
+		existing.setInscricoes(v.getInscricoes());
+		existing.setUtilizador(v.getUtilizador());
+		
+		return voluntarioRepository.save(existing);
+	}
+	
+	public void deleteVoluntario(Long id) {
+		voluntarioRepository.deleteById(id);
+	}
+	
+
+}
