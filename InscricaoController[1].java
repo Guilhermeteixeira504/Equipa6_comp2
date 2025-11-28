14560abf031dfce1a174826f494ac3289331b1f9
Bruninha9 <brunajasmin15@gmail.com>
2025-11-28 14:32:20 +0000

    inscricao controller

---
+++ InscricaoController[1].java
@@ -0,0 +1,43 @@
+package lp.Equipa6_Comp2.controller;
+
+import org.springframework.web.bind.annotation.*;
+import lp.Equipa6_Comp2.entity.Inscricao;
+import lp.Equipa6_Comp2.service.InscricaoService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/voluntariado/inscricoes")
+public class InscricaoController {
+	
+	private final InscricaoService inscricaoService;
+	
+	public InscricaoController(InscricaoService inscricaoService) {
+		this.inscricaoService = inscricaoService;
+	}
+	
+	@GetMapping
+	public List<Inscricao> getAll() {
+		return inscricaoService.getAllInscricoes();
+	}
+	
+	@GetMapping("/{id}")
+	public Inscricao getById(@PathVariable Long id) {
+		return inscricaoService.getInscricao(id);
+	}
+	
+	@PostMapping
+	public Inscricao create(@RequestBody Inscricao insc) {
+		return inscricaoService.createInscricao(insc);
+	}
+	
+	@PutMapping("/{id}")
+	public Inscricao update(@PathVariable Long id, @RequestBody Inscricao insc) {
+		return inscricaoService.updateInscricao(id, insc);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		inscricaoService.deleteInscricao(id);
+	}
+	
+}
