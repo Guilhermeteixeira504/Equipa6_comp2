85600cbba44aa1baac13127d0c857c8e29116482
Bruninha9 <brunajasmin15@gmail.com>
2025-11-28 14:33:56 +0000

    inscricao service

---
+++ InscricaoService[1].java
@@ -0,0 +1,42 @@
+package lp.Equipa6_Comp2.service;
+
+import org.springframework.stereotype.Service;
+import lp.Equipa6_Comp2.entity.Inscricao;
+import lp.Equipa6_Comp2.repository.InscricaoRepository;
+import java.util.List;
+
+@Service
+public class InscricaoService {
+	
+	private final InscricaoRepository inscricaoRepository;
+	
+	public InscricaoService(InscricaoRepository inscricaoRepository) {
+		this.inscricaoRepository = inscricaoRepository;
+	}
+	
+	public List<Inscricao> getAllInscricoes(){
+		return inscricaoRepository.findAll();
+	}
+	
+	public Inscricao getInscricao(Long id) {
+		return inscricaoRepository.findById(id).orElse(null);
+	}
+	
+	public Inscricao createInscricao(Inscricao insc) {
+		return inscricaoRepository.save(insc);
+	}
+	
+	public Inscricao updateInscricao(Long id, Inscricao insc) {
+		Inscricao existing = inscricaoRepository.findById(id).orElseThrow(() -> new RuntimeException("Inscrição não encontrada"));
+		existing.setVoluntario(insc.getVoluntario());
+		existing.setPrograma(insc.getPrograma());
+		existing.setNHorasRealizadas(insc.getNHorasRealizadas());
+		
+		return inscricaoRepository.save(existing);
+	}
+	
+	public void deleteInscricao(Long id) {
+		inscricaoRepository.deleteById(id);
+	}
+
+}
